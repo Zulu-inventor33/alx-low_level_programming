1b0695ff88ed9eddca209af60e5574f06739226b
Zulu-inventor33 <muteakelvin15@gmail.com>
2023-08-18 04:00:30 -0700

    fabulous

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - The function draws a continous line on the terminal
+ * @n: number of times the character _ should be printed
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+			_putchar('\n');
+	}
+}
