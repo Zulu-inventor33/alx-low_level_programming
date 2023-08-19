d1143242ede4fcc07fb66c0d48369de201c011aa
Zulu-inventor33 <muteakelvin15@gmail.com>
2023-08-18 19:48:30 -0700

    corrections

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - function prints the numbrs from 1-100, but for fmultiple numbers of 3
+ * Fizz is printed instead of the number, for multiple numbers of five
+ * Buzz, and for multiple of both three and five
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("Fizzbuzz");
+
+		else if ((num % 3) == 0)
+			printf("Fizz");
+
+		else if ((num % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
+		if (num == 100)
+			continue;
+		printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
