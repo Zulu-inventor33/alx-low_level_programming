507482891cb17cb480debe7bf7510cfa7278e09c
Zulu-inventor33 <muteakelvin15@gmail.com>
2023-08-19 23:59:08 -0700

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,17 +2,32 @@
 
 /**
  * print_triangle - function prints a triangle, using the character #.
+ * Description: i prints diagonals
  * @size: The size of the triangle.
+ * Return: Void
  */
 
-void print_trinangle(int size)
+void print_triangle(int size)
 {
-	int hash, index;
+	int row, hashes, spaces;
 
-	if (size > 0)
+	if (size <= 0)
 	{
-		for (hash = 1 ; hash <= size; hash++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
 		{
-			_putchar(' ');
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		for (index = 
+	}
+}
