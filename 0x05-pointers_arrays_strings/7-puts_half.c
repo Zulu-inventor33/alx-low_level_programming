1c86b897b9aa7c9032194b3832530d789ad1a580
Zulu-inventor33 <muteakelvin15@gmail.com>
2023-08-21 15:08:35 -0700

    number 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_ahalf - prints ahalf of a string
+ * @str: input
+ */
+void puts_ahalf(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		lenn++;
+		str++;
+	}
+	str -= (len / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+
+	_putchar('\n');
+}
