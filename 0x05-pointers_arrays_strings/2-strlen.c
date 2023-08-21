d1b9fea0d88b879ad0da4e1a57b5a3d381db0ec2
Zulu-inventor33 <muteakelvin15@gmail.com>
2023-08-21 13:02:47 -0700

    number 3 commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of the string
+ * @s:-string to be checked
+ * Return: The length of the string
+ */
+
+int _strlen(char *s)
+{
+int string_length = 0;
+	while (s[string length])
+		string length++;
+	return (string_length);
+}
