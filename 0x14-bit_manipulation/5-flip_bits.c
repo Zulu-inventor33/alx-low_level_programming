ef62752412aab8be4546aeeb73c4ddd88bdda252
Zulu-inventor33 <muteakelvin15@gmail.com>
2023-09-28 19:58:52 -0700

    number 5 correction

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * flip_bits - this counts the number of bits to change to get from one number to the other
+ * flip_bits - this counts the number 
+ * of bits to change to get from
+ * one number to the other
  * @n: the first number
  * @m: the second number
  *
